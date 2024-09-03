1ace3a595611b01965ce369fa9b62aa46309719c
KULBHUSHANSINGH-arch <gautamenergy526@gmail.com>
2024-09-03 23:34:49 +0530

    update some

---
+++ Pattern/Function/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the Value of n" << endl;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Kulbhushan Singh" << endl;
+    }
+    // cout<<
+}
